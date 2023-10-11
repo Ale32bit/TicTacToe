bfd1b39aa7fcafd7635ffc177f0f5f0183cd37b8
Tsadriu <40073056+Tsadriu@users.noreply.github.com>
2023-10-11 15:52:54 +0200

    Aggiunti colori:  - DarkBlue  - LightBlue  - White
     Aggiunto Font:
      - righteous_regular.ttf

--- app/src/main/java/me/alexdevs/tictactoe/ui/theme/Theme.kt
+++ app/src/main/java/me/alexdevs/tictactoe/ui/theme/Theme.kt
@@ -16,17 +16,20 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = DarkBlue,
+    secondary = LightBlue,
+    tertiary = White,
+    background = DarkBlue,
+
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = DarkBlue,
+    secondary = LightBlue,
+    tertiary = White,
+    background = DarkBlue,
 
-    /* Other default colors to override
+    /*Other default colors to override
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
     onPrimary = Color.White,
