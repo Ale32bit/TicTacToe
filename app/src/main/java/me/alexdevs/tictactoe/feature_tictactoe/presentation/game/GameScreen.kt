d359028d5fe2f5e39bf0868ce9b970d250b02598
Alessandro Proto <me@alexdevs.me>
2023-10-13 10:34:22 +0200

    Remove log

--- app/src/main/java/me/alexdevs/tictactoe/feature_tictactoe/presentation/game/GameScreen.kt
+++ app/src/main/java/me/alexdevs/tictactoe/feature_tictactoe/presentation/game/GameScreen.kt
@@ -43,8 +43,6 @@ fun GameScreen(
     val state = viewModel.state.value
     val winnerName = state.gameRound.winner
 
-    Log.d("GameScreen", mode.toString())
-
     /**
      * NON TOCCARE !!!!!!!!
      */
